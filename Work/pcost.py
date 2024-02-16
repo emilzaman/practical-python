7473040939aba190cae69fb4bada2997496e7a53
Emil Zaman <emil_zaman@yahoo.com>
2024-02-16 15:49:45 +0200

    update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,9 @@
 # pcost.py
 #
 # Exercise 1.27
+suma = 0.0
+with open('Work/Data/portfolio.csv','rt') as f:
+    next(f)
+    for line in f:
+        suma=suma+float(line.split(',')[2])*float(line.split(',')[1])
+print (f'Total cost is: {suma:.2f}')
